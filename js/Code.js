d78688ce6cdd3f00d2f7e8677709795a3438576a
Vishvam-03 <75116813+Vishvam-03@users.noreply.github.com>
2021-05-25 09:54:07 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    // Clue for Q-1
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    // Clue for Q-2
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700, 150)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 700, 170)
+    
+    // Clue for Q-3
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100, 300)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100, 320)
+}
